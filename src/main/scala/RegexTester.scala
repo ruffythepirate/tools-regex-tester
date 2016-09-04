7fdd23e537e0a4ce443b3aaa8858f3e7a6e9aa95
Ruffy <johan.ellfries@gmail.com>
2016-09-04 21:27:00 +0200

    Initial commit

---
+++ src/main/scala/RegexTester.scala
@@ -0,0 +1,89 @@
+//package io.github.ruffy.tools
+
+object RegexTester extends App {
+
+  val mainWindow = new MainFrame()
+
+  mainWindow.show
+}
+
+import javax.swing._
+import java.awt.BorderLayout
+import javax.swing.event.{DocumentListener, DocumentEvent}
+
+class MainFrame extends JFrame("Regex Tester") {
+
+  setSize(800, 500)
+
+
+    val splitPane = new JSplitPane
+    val regexTxt = new JTextField
+    val inputTextTxt = new JTextArea
+    val leftPanel = new JPanel(new BorderLayout)
+    val matchesTxt = new JTextArea
+
+  initializeComponents
+
+
+  private def initializeComponents = {
+
+    splitPane.setDividerLocation(300)
+
+    regexTxt.getDocument.addDocumentListener(new CallbackListener( de => {
+      updateRegex
+      updateMatches
+    }));
+
+    inputTextTxt.getDocument.addDocumentListener(new CallbackListener( de => {
+      updateMatches
+    }));
+
+
+    leftPanel.add(regexTxt, BorderLayout.NORTH)
+    leftPanel.add(inputTextTxt, BorderLayout.CENTER)
+
+    splitPane.setLeftComponent(leftPanel)
+
+    splitPane.setRightComponent(matchesTxt)
+
+    add(splitPane)
+  }
+
+  private def updateMatches() {
+
+    val inputText = inputTextTxt.getText
+    val matches = _regex findAllIn inputText toArray
+
+    val matchString = matches.mkString("\n")
+    matchesTxt.setText(matchString)
+  }
+
+  private var _regex = "".r
+  private var _lastRegexString= ""
+
+  private def updateRegex() {
+    if(_lastRegexString != regexTxt.getText) {
+      try{
+        _lastRegexString = regexTxt.getText
+        _regex = _lastRegexString.r.unanchored
+      } catch {
+        case _ :Throwable => 
+      }
+    }
+  }
+
+
+  class CallbackListener(callback: DocumentEvent => Unit) extends DocumentListener {
+    def changedUpdate(de : DocumentEvent) {
+      callback(de)
+    }
+
+    def insertUpdate(de : DocumentEvent) {
+      callback(de)
+    }
+
+    def removeUpdate(de : DocumentEvent) {
+      callback(de)
+    }
+  }
+}
